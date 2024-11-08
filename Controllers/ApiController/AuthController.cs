0bfbb6d5924c2e4cf30a3273340e51286121bf23
hiuphn <phamhuynhngochieu468@gmail.com>
2024-11-08 20:49:24 +0700

    ád

--- Controllers/ApiController/AuthController.cs
+++ Controllers/ApiController/AuthController.cs
@@ -108,9 +108,9 @@ namespace WebQuanLyNhaKhoa.Controllers.ApiConrtroller
         {
             var user = await _context.TaiKhoans.SingleOrDefaultAsync(u => u.TenDangNhap == loginRequest.TenDangNhap);
 
-            if (user == null || !BCrypt.Net.BCrypt.Verify(loginRequest.MatKhau, user.MatKhau))
+            if (user == null || !BCrypt.Net.BCrypt.Verify(loginRequest.MatKhau, user.MatKhau) || user.isLoocked == true)
             {
-                ModelState.AddModelError("", "Sai tên đăng nhập hoặc mật khẩu.");
+                ModelState.AddModelError("", "Sai tên đăng nhập hoặc mật khẩu. Hoặc tài khoản đã bị vô hiệu hóa");
                 return View(loginRequest);
             }
 
