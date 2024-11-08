db009bf757b71411f1d9170453fd29b4b5fe5c15
hiuphn <phamhuynhngochieu468@gmail.com>
2024-11-08 16:36:57 +0700

    alo

--- Controllers/ApiController/BenhNhanController.cs
+++ Controllers/ApiController/BenhNhanController.cs
@@ -179,12 +179,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System;
+using WebQuanLyNhaKhoa.DTO;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WebQuanLyNhaKhoa.Controllers.ApiConrtroller
 {
     [Authorize]
     [Route("[controller]")]
-    
+
     public class BenhNhanController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -193,15 +195,20 @@ namespace WebQuanLyNhaKhoa.Controllers.ApiConrtroller
         {
             _context = context;
         }
+        [Authorize(Roles = "Admin")]
+        [HttpGet("Index")]
+        public IActionResult Index()
+        {
 
+            return View();
+        }
         // GET: api/BenhNhan
-        // Only accessible by Admin role
         [Authorize(Roles = "Admin")]
         [HttpGet("api/GetBenhNhan")]
-        public async Task<ActionResult<IEnumerable<BenhNhan>>> GetBenhNhans(string query = "", string filter = "nothing")
+        public async Task<ActionResult<IEnumerable<BenhNhanDTO>>> GetBenhNhans(string query = "", string filter = "nothing")
         {
             var currentDay = DateTime.Now;
-            IQueryable<BenhNhan> queryable = _context.BenhNhans;
+            var queryable = _context.BenhNhans.AsQueryable();
 
             if (!string.IsNullOrEmpty(query))
             {
@@ -222,13 +229,26 @@ namespace WebQuanLyNhaKhoa.Controllers.ApiConrtroller
                     break;
             }
 
-            return Ok(await queryable.ToListAsync());
+            var result = await queryable
+                .Select(n => new BenhNhanDTO
+                {
+                    IdbenhNhan = n.IdbenhNhan,
+                    HoTen = n.HoTen,
+                    Gioi = n.Gioi,
+                    Sdt = n.Sdt,
+                    NamSinh = n.NamSinh,
+                    DiaChi = n.DiaChi,
+                    NgayKhamDau = n.NgayKhamDau
+                })
+                .ToListAsync();
+
+            return Ok(result);
         }
 
         // GET: api/BenhNhan/{id}
         [Authorize(Roles = "Admin,Staff")]
         [HttpGet("api/GetBenhNhan/{id}")]
-        public async Task<ActionResult<BenhNhan>> GetBenhNhan(int id)
+        public async Task<ActionResult<BenhNhanDTO>> GetBenhNhan(int id)
         {
             var benhNhan = await _context.BenhNhans.FindAsync(id);
 
@@ -269,6 +289,12 @@ namespace WebQuanLyNhaKhoa.Controllers.ApiConrtroller
             return Ok();
         }
 
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
         // POST: api/BenhNhan
         [Authorize(Roles = "Admin,Staff")]
         [HttpPost("api/PostBenhNhan")]
@@ -285,20 +311,64 @@ namespace WebQuanLyNhaKhoa.Controllers.ApiConrtroller
             return CreatedAtAction(nameof(GetBenhNhan), new { id = benhNhan.IdbenhNhan }, benhNhan);
         }
 
+        [Authorize(Roles = "Admin,Staff")]
+        [HttpGet("Edit/{id}")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var benhNhan = await _context.BenhNhans.FindAsync(id); // Retrieve BenhNhan by ID
+
+            if (benhNhan == null)
+            {
+                return NotFound();
+            }
+
+            // Map BenhNhan to BenhNhanDTO
+            var benhNhanDto = new BenhNhanDTO
+            {
+                IdbenhNhan = benhNhan.IdbenhNhan,
+                HoTen = benhNhan.HoTen,
+                Gioi = benhNhan.Gioi,
+                NamSinh = benhNhan.NamSinh,
+                Sdt = benhNhan.Sdt,
+                DiaChi = benhNhan.DiaChi,
+                NgayKhamDau = benhNhan.NgayKhamDau
+            };
+
+            // Pass BenhNhanDTO to the view
+            return View(benhNhanDto);
+        }
+
         // PUT: api/BenhNhan/{id}
         [Authorize(Roles = "Admin,Staff")]
         [HttpPut("api/PutBenhNhan/{id}")]
-        public async Task<IActionResult> PutBenhNhan(int id, BenhNhan benhNhan)
+        public async Task<IActionResult> PutBenhNhan(int id, [FromBody] BenhNhanDTO benhNhanDto)
         {
-            if (id != benhNhan.IdbenhNhan)
+            if (id == null)
             {
-                return BadRequest();
+                return BadRequest("The ID in the URL does not match the ID in the payload.");
             }
 
+            // Find the existing BenhNhan entity
+            var benhNhan = await _context.BenhNhans.FirstOrDefaultAsync(n => n.IdbenhNhan == id);
+            if (benhNhan == null)
+            {
+                return NotFound();
+            }
+
+            // Map fields from BenhNhanDTO to BenhNhan
+            benhNhan.HoTen = benhNhanDto.HoTen;
+            benhNhan.Gioi = benhNhanDto.Gioi;
+            benhNhan.NamSinh = benhNhanDto.NamSinh;
+            benhNhan.Sdt = benhNhanDto.Sdt;
+            benhNhan.DiaChi = benhNhanDto.DiaChi;
+            benhNhan.NgayKhamDau = benhNhanDto.NgayKhamDau;
+
+            // Mark entity as modified
             _context.Entry(benhNhan).State = EntityState.Modified;
 
             try
             {
+                // Save changes to the database
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
@@ -315,6 +385,7 @@ namespace WebQuanLyNhaKhoa.Controllers.ApiConrtroller
 
             return NoContent();
         }
+
 
         // DELETE: api/BenhNhan/{id}
         [Authorize(Roles = "Admin")]
@@ -342,105 +413,105 @@ namespace WebQuanLyNhaKhoa.Controllers.ApiConrtroller
 
         /////////////////////////////////////////////////////////////////////
         // GET: BenhNhan/Index (MVC View)
-        [HttpGet]
-        [Authorize(Roles = "Admin,Staff")]
-        public async Task<IActionResult> Index()
-        {
-            var benhNhans = await _context.BenhNhans.ToListAsync();
-            return View(benhNhans);
-        }
+        //[HttpGet]
+        //[Authorize(Roles = "Admin,Staff")]
+        //public async Task<IActionResult> Index()
+        //{
+        //    var benhNhans = await _context.BenhNhans.ToListAsync();
+        //    return View(benhNhans);
+        //}
 
-        // GET: BenhNhan/Details/5 (MVC View)
-        [HttpGet("Details/{id:int}")]
-        [Authorize(Roles = "Admin,Staff")]
-        public async Task<IActionResult> Details(int id)
-        {
-            var benhNhan = await _context.BenhNhans.FindAsync(id);
-            if (benhNhan == null) return NotFound();
-            return View(benhNhan);
-        }
+        //// GET: BenhNhan/Details/5 (MVC View)
+        //[HttpGet("Details/{id:int}")]
+        //[Authorize(Roles = "Admin,Staff")]
+        //public async Task<IActionResult> Details(int id)
+        //{
+        //    var benhNhan = await _context.BenhNhans.FindAsync(id);
+        //    if (benhNhan == null) return NotFound();
+        //    return View(benhNhan);
+        //}
 
-        // GET: BenhNhan/Create (MVC View)
-        [HttpGet("Create")]
-        [Authorize(Roles = "Admin,Staff")]
-        public IActionResult Create()
-        {
-            return View();
-        }
+        //// GET: BenhNhan/Create (MVC View)
+        //[HttpGet("Create")]
+        //[Authorize(Roles = "Admin,Staff")]
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // POST: BenhNhan/Create (MVC View)
-        [HttpPost("Create")]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin,Staff")]
-        public async Task<IActionResult> Create(BenhNhan benhNhan)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(benhNhan);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(benhNhan);
-        }
+        //// POST: BenhNhan/Create (MVC View)
+        //[HttpPost("Create")]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin,Staff")]
+        //public async Task<IActionResult> Create(BenhNhan benhNhan)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(benhNhan);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(benhNhan);
+        //}
 
-        // GET: BenhNhan/Edit/5 (MVC View)
-        [HttpGet("Edit/{id:int}")]
-        [Authorize(Roles = "Admin,Staff")]
-        public async Task<IActionResult> Edit(int id)
-        {
-            var benhNhan = await _context.BenhNhans.FindAsync(id);
-            if (benhNhan == null) return NotFound();
-            return View(benhNhan);
-        }
+        //// GET: BenhNhan/Edit/5 (MVC View)
+        //[HttpGet("Edit/{id:int}")]
+        //[Authorize(Roles = "Admin,Staff")]
+        //public async Task<IActionResult> Edit(int id)
+        //{
+        //    var benhNhan = await _context.BenhNhans.FindAsync(id);
+        //    if (benhNhan == null) return NotFound();
+        //    return View(benhNhan);
+        //}
 
-        // POST: BenhNhan/Edit/5 (MVC View)
-        [HttpPost("Edit/{id:int}")]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin,Staff")]
-        public async Task<IActionResult> Edit(int id, BenhNhan benhNhan)
-        {
-            if (id != benhNhan.IdbenhNhan) return BadRequest();
+        //// POST: BenhNhan/Edit/5 (MVC View)
+        //[HttpPost("Edit/{id:int}")]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin,Staff")]
+        //public async Task<IActionResult> Edit(int id, BenhNhan benhNhan)
+        //{
+        //    if (id != benhNhan.IdbenhNhan) return BadRequest();
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(benhNhan);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BenhNhanExists(id)) return NotFound();
-                    throw;
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(benhNhan);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(benhNhan);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!BenhNhanExists(id)) return NotFound();
+        //            throw;
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(benhNhan);
+        //}
 
-        // GET: BenhNhan/Delete/5 (MVC View)
-        [HttpGet("Delete/{id:int}")]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var benhNhan = await _context.BenhNhans.FindAsync(id);
-            if (benhNhan == null) return NotFound();
-            return View(benhNhan);
-        }
+        //// GET: BenhNhan/Delete/5 (MVC View)
+        //[HttpGet("Delete/{id:int}")]
+        //[Authorize(Roles = "Admin")]
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    var benhNhan = await _context.BenhNhans.FindAsync(id);
+        //    if (benhNhan == null) return NotFound();
+        //    return View(benhNhan);
+        //}
 
-        // POST: BenhNhan/Delete/5 (MVC View)
-        [HttpPost("Delete/{id:int}"), ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var benhNhan = await _context.BenhNhans.FindAsync(id);
-            if (benhNhan != null)
-            {
-                _context.BenhNhans.Remove(benhNhan);
-                await _context.SaveChangesAsync();
-            }
-            return RedirectToAction(nameof(Index));
-        }
+        //// POST: BenhNhan/Delete/5 (MVC View)
+        //[HttpPost("Delete/{id:int}"), ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin")]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var benhNhan = await _context.BenhNhans.FindAsync(id);
+        //    if (benhNhan != null)
+        //    {
+        //        _context.BenhNhans.Remove(benhNhan);
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    return RedirectToAction(nameof(Index));
+        //}
     }
 }
