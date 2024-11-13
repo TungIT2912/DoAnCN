52ede10fe6fe53e42662aa9e9d4408d05bfa364f
aycifym8 <pquangngo@gmail.com>
2024-11-13 14:48:14 +0700

    ok đc cái table pricing với làm đẹp thêm tí cái table, còn lại nốt sau

--- Controllers/HomePageCustomer/ServicesController.cs
+++ Controllers/HomePageCustomer/ServicesController.cs
@@ -1,33 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebQuanLyNhaKhoa.Data;
+using X.PagedList;
+using X.PagedList.Extensions;  // Make sure to include this namespace
 
 namespace WebQuanLyNhaKhoa.Controllers.HomePageCustomer
 {
-   public class ServicesController : Controller
-   {
-       private readonly ApplicationDbContext _context;
+    public class ServicesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
 
-       public ServicesController(ApplicationDbContext context)
-       {
-           _context = context;
-       }
+        public ServicesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-       // GET: Services
-       public async Task<IActionResult> Index()
-       {
-             var qlnhaKhoaContext = _context.DichVus;
+        // GET: Services
+        public async Task<IActionResult> Index()
+        {
+            var qlnhaKhoaContext = _context.DichVus;
             return View(await qlnhaKhoaContext.ToListAsync());
-       }
+        }
 
-       	public IActionResult Pricing()
-		{
-			return View();
-		}
-   }
+        public ActionResult Pricing(int? page)
+        {
+            int pageSize = 4;  
+            int pageNumber = (page ?? 1);  // Default to page 1 if no page parameter
+
+            // Use AsQueryable() to ensure that ToPagedList can be applied
+            var pricingData = _context.DichVus.AsQueryable();  // Convert to IQueryable
+
+            return View(pricingData.ToPagedList(pageNumber, pageSize)); // Use ToPagedList from X.PagedList
+        }
+    }
 }
