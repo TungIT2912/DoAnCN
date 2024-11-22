f226cb4676cf7aea341f9d2df833b15632836b0f
aycifym8 <pquangngo@gmail.com>
2024-11-22 11:28:40 +0700

    sửa lỗi, thêm doctor info + add được bệnh nhân!

--- Controllers/HomePageCustomer/BookAppointmentController.cs
+++ Controllers/HomePageCustomer/BookAppointmentController.cs
@@ -6,7 +6,7 @@ using WebQuanLyNhaKhoa.DTO;
 
 namespace WebQuanLyNhaKhoa.Controllers.HomePageCustomer
 {
-   [Route("[controller]")]
+   [Route("customer/controller")]
    public class BookAppointmentController : Controller
    {
        private readonly ApplicationDbContext _context;
@@ -15,14 +15,14 @@ namespace WebQuanLyNhaKhoa.Controllers.HomePageCustomer
        {
            _context = context;
        }
-       [Authorize(Roles = "Admin")]
+    //    [Authorize(Roles = "Admin")]
        [HttpGet("Index")]
        public async Task<IActionResult> Index()
        {
            return View();
        }
 
-       [Authorize(Roles = "Admin")]
+    //    [Authorize(Roles = "Admin")]
        [HttpGet("api/GetBenhNhan")]
        public async Task<ActionResult<IEnumerable<BenhNhanDTO>>> GetBenhhNhans()
        {
@@ -49,7 +49,7 @@ namespace WebQuanLyNhaKhoa.Controllers.HomePageCustomer
            return View();
        }
        // POST: api/BenhNhan
-       [Authorize(Roles = "Admin,Staff")]
+    //    [Authorize(Roles = "Admin,Staff")]
        [HttpPost("api/PostBenhNhan")]
        public async Task<ActionResult<BenhNhan>> PostBenhNhan([FromBody] BenhNhan benhNhan)
        {
--- Controllers/HomePageCustomer/DoctorController.cs
+++ Controllers/HomePageCustomer/DoctorController.cs
@@ -1,13 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebQuanLyNhaKhoa.Data;
 
 public class DoctorController : Controller
 {
+    private readonly ApplicationDbContext _context;
+
+    public DoctorController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public ActionResult Doctors()
     {
-        return View();
+       
+        var doctors = _context.NhanViens
+            .Where(nv => nv.ChucVu.TenCv == "Doctor") 
+            .ToList();
+
+        return View(doctors); 
     }
-    public ActionResult DoctorDetails()
+
+    public ActionResult DoctorDetails(int id)
     {
-        return View();
+        // Fetch details of a specific doctor by their ID
+        var doctor = _context.NhanViens
+            .FirstOrDefault(nv => nv.MaNv == id);
+
+        if (doctor == null)
+        {
+            return NotFound(); // Return 404 if not found
+        }
+
+        return View(doctor); // Pass the doctor details to the view
     }
 }
